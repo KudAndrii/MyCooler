3370cf56b95c56253341055a197e15548a5a5575
Andrii Kud <cud.andrei@gmail.com>
2026-06-14 19:10:18 +0300

    Initial Commit

---
+++ my-cooler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  my-cooler
+//
+//  Created by Andrei Kud on 14.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ my-cooler/my_coolerApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_coolerApp.swift
+//  my-cooler
+//
+//  Created by Andrei Kud on 14.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct my_coolerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
